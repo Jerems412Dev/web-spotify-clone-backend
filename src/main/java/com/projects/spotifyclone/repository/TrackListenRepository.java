fa8b54ea4f96b9202a4a10442a7a1d9ed8fb9f26
jeremyobiangdev <jeremyobiangdev@gmail.com>
2024-06-06 01:16:06 +0100

    feat: add find trackListen by username 2.

--- src/main/java/com/projects/spotifyclone/repository/TrackListenRepository.java
+++ src/main/java/com/projects/spotifyclone/repository/TrackListenRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface TrackListenRepository extends JpaRepository<TrackListenEntity, Integer> {
     TrackListenEntity findTopByUser_UsernameOrderByIdDesc(String user);
-    List<TrackListenEntity> findTop8ByUser_UsernameOrderByIdDesc(String username);
+    List<TrackListenEntity> findDistinctTop8ByUser_UsernameOrderByIdDesc(String username);
     List<TrackListenEntity> findDistinctByUser_UsernameAndTrack_TitleTrack(String username, String titleTrack);
     List<TrackListenEntity> findDistinctByUser_UsernameAndTrack_TitleTrackNot(String username, String titleTrack);
     Boolean existsDistinctByUser_UsernameAndTrack_TitleTrack(String username, String titleTrack);
