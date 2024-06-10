38bf4ab3844093c04656273763ed43609c73a9f0
jeremyobiangdev <jeremyobiangdev@gmail.com>
2024-06-10 07:02:26 +0100

    feat: edit find track listen by username repository 3.

--- src/main/java/com/projects/spotifyclone/service/TrackListenService.java
+++ src/main/java/com/projects/spotifyclone/service/TrackListenService.java
@@ -46,7 +46,7 @@ public class TrackListenService {
     //find all trackListens by username
     @Transactional(readOnly = true)
     public List<TrackListenDTO> findByUsername(String username) {
-        return tracklistenMapper.tracklistenEntityListToTrackListenDTOList(tracklistenRepository.findTop8ByUser_Username(username));
+        return tracklistenMapper.tracklistenEntityListToTrackListenDTOList(tracklistenRepository.findByUser_Username(username));
     }
 
     // check if a track has been listened to by a user
